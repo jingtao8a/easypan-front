40d5f5e47b7ad99aac778bc0aa21ddaead58491e
yuxintao <1921056015@qq.com>
2024-09-04 10:56:23 +0800

    初始化工程

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+//引入element plus
+import ElementPlus from  'element-plus'
+import 'element-plus/dist/index.css'
+//图标
+import '@/assets/icon/iconfont.css'
+import '@/assets/base.scss'
+//引入cookies
+import VueCookies from 'vue-cookies'
 
 const app = createApp(App)
-
+app.use(ElementPlus)
 app.use(router)
 
 app.mount('#app')
---
+++ src/views/Test.vue
@@ -0,0 +1,12 @@
+<template>
+    <!-- input输入 -->
+     <el-form-item label="" prop="">
+        <el-input clearable placeholder="提示信息"></el-input>
+     </el-form-item>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
